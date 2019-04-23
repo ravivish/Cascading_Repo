c268c0e227e37c0cf3e774c37d47d72a2177bd95
ravivish <ravivishwakarma825@gmail.com>
2019-04-23 16:00:47 +0530

    Initia commit

---
+++ Cascading DropDown.aspx
@@ -0,0 +1,80 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Cascading DropDown.aspx.cs" Inherits="Test16_1_19.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <table>
+            <tr>
+                <td>First Name:</td>
+                <td>
+                    <asp:TextBox ID="txtfname" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+
+            <tr>
+                <td>Last Name:</td>
+                <td>
+                    <asp:TextBox ID="txtlname" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+
+            <tr>
+                <td>Country:</td>
+                <td>
+                    <asp:DropDownList ID="ddlcountry" runat="server"></asp:DropDownList>
+                </td>
+            </tr>
+            <tr>
+                <td>Department</td>
+                <td>
+                    <asp:DropDownList ID="ddldepart" runat="server"></asp:DropDownList>
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
+                    <asp:Button ID="btnsave" runat="server" Text="Save" OnClick="btnsave_Click" />
+                </td>
+            </tr>
+
+            <tr>
+                <td style="text-decoration: underline">
+                    <asp:GridView ID="grd" runat="server" AutoGenerateColumns="false">
+                        <Columns>
+                            <asp:TemplateField HeaderText="Fname">
+                                <ItemTemplate>
+                                    <%#Eval("fname") %>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                            <asp:TemplateField HeaderText="Lname ">
+                                <ItemTemplate>
+                                    <%#Eval("lname") %>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                            <asp:TemplateField HeaderText="Country">
+                                <ItemTemplate>
+                                    <%#Eval("cname") %>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                            <asp:TemplateField HeaderText="Department">
+                                <ItemTemplate>
+                                    <%#Eval("dname") %>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                        </Columns>
+                    </asp:GridView>
+                </td>
+            </tr>
+
+        </table>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Cascading DropDown.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Test16_1_19
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["con"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                show();
+                bindcountry();
+                binddepartment();
+            }
+
+        }
+        private void show()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("usp_reg_join", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            con.Close();
+            grd.DataSource = ds;
+            grd.DataBind();
+        }
+        private void bindcountry()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("usp_country_select", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            con.Close();
+            ddlcountry.DataValueField = "id";
+            ddlcountry.DataTextField = "cname";
+            ddlcountry.DataSource = ds;
+            ddlcountry.DataBind();
+            ddlcountry.Items.Insert(0, new ListItem("--Select--", "0"));
+        }
+        private void binddepartment()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("usp_depart_select", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            con.Close();
+            ddldepart.DataValueField = "id";
+            ddldepart.DataTextField = "dname";
+            ddldepart.DataSource = ds;
+            ddldepart.DataBind();
+            ddldepart.Items.Insert(0, new ListItem("--Select--", "0"));
+        }
+
+        protected void btnsave_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("usp_reg_insert", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@fname", txtfname.Text);
+            cmd.Parameters.AddWithValue("@lname", txtlname.Text);
+            cmd.Parameters.AddWithValue("@country", ddlcountry.SelectedValue);
+            cmd.Parameters.AddWithValue("@depart", ddldepart.SelectedValue);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            show();
+        }
+    }
+}
